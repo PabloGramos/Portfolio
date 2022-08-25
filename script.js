dc0b3a46963133bfbf97df4f905c979574de57a3
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2022-08-25 17:04:24 -0300

    Adicionado formulário

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function clicar(){
-    var a = window.document.getElementsById('area')
-    a.innerText = 'Clicou'
-}
-function entrar(){
-    var b = window
+function Enviar(){
+    var nome = document.getElementById("nomeid")
+    if(nome.value != ""){
+        alert("Obrigado " + nome.value + ", seus dados foram encaminhados com sucesso.")
+    }
 }
