61fac2460ba92d695700e97707d7f52528fc1162
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2022-08-30 16:14:07 -0300

    Atualizando texto

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function Enviar(){
     var nome = document.getElementById("nomeid")
+    var fone = document.getElementById("foneid")
+    var email = document.getElementById("emailid")
+    var msg = document.getElementById("msgid")
+
     if(nome.value != ""){
         alert("Obrigado " + nome.value + ", seus dados foram encaminhados com sucesso.")
+        alert( nome.value + "\n" + fone.value + "\n" + email.value + "\n" + msg)
     }
 }
