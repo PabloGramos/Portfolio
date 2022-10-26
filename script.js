f244d1afc092349c9145e5bc6edb8ff7d6886fcd
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2022-10-25 23:02:09 -0300

    Atualizando
    Tentando enviar os dados no email

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function Enviar(){
     var nome = document.getElementById("nomeid")
     var fone = document.getElementById("foneid")
@@ -6,8 +7,7 @@ function Enviar(){
 
     if(nome.value != ""){
         alert("Obrigado " + nome.value + ", seus dados foram encaminhados com sucesso.")
-        alert( nome.value + "\n" + fone.value + "\n" + email.value + "\n" + msg)
     }
 }
-var a = 30
-console.log("sakjdhsakjhj")
+
+*/
