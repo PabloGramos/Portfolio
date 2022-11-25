ef1c832702e1231b6728654819a398a55094519c
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2022-11-25 17:21:21 -0300

    Atualizado e iniciado novo projeto
    Adicionei animações em Javascript em partes da home page e iniciei o novo projeto de album.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 function Enviar(){
     var nome = document.getElementById("nomeid")
     var fone = document.getElementById("foneid")
@@ -10,3 +10,19 @@ function Enviar(){
         window.location.reload(true)
     }
 }
+*/
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.texto', {duration: 3000});
+sr.reveal('.titulo_conhecimento', {
+    rotate: {x:100, y:0, z:0},
+    duration: 2000
+});
+sr.reveal('.tecnologia', {
+    rotate: {x:0, y:100, z:200},
+    duration: 2500
+});
+sr.reveal('.texto2', {
+    duration: 2500
+});
